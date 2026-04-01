ba02da2945b7d1e695e0d4ebf309e5b7e77cca6c
wrrrswrr <112405025@g.nccu.edu.tw>
2026-04-01 21:37:49 +0800

    feat: new

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image"; 
+
+export default function About() {
+  return (
+    <>
+      About 關於我
+    </>
+  );
+}
---
+++ app/hobby/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image"; 
+
+export default function About() {
+  return (
+    <>
+      Hobby 我的興趣
+    </>
+  );
+}
